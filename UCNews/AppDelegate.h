42efc2d0c14a52a6e88ee76e15affe710dc60a99
wc503958469 <503958469@qq.com>
2015-10-13 08:11:36 +0800

    Initial Commit

---
+++ UCNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UCNews
+//
+//  Created by wangchen on 15/10/13.
+//  Copyright © 2015年 chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
